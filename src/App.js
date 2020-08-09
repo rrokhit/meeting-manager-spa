0bc020f4959c81fc2ae22600f06bbe7729b244bc
Ronen Rokhit <r.rokhit11@gmail.com>
2020-08-09 00:23:09 -0400

    added welcome statement

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import Welcome from './Welcome';
 
 class App extends Component {
 
 constructor() {
   super();
   this.state = {
-    user : null
+    user : 'null'
   }
 }
 
   render() {
-    return <Home user={this.state.user}/>;
+    return(
+      <div>
+        {this.state.user &&
+      <Welcome user={this.state.user}/>}
+      <Home user={this.state.user}/>
+      </div>
+    ) ;
   }
 }
 
