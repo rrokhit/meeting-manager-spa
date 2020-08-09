b12fe2571aa0a45fc064e5cbe01a86e4fa042047
Ronen Rokhit <r.rokhit11@gmail.com>
2020-08-08 23:47:45 -0400

    conditionally shows login/register button

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 
-class Home extends React.Component {
+class Home extends Component {
     render() {
+
+      const {user} = this.props;
+
         return(
             <div className="container text-center">
             <div className="row justify-content-center">
               <div className="col-10 col-md-10 col-lg-8 col-xl-7">
+                Hello {user}
                 <div className="display-4 text-primary mt-3 mb-2">
                   Meeting Log
                 </div>
@@ -14,19 +18,27 @@ class Home extends React.Component {
                   in, and picks random users to award giveaways. It's a
                   good example of a Single Page Application which includes
                   connection to a database and routing. It's a practical
-                  way to learn <a href="https://reactjs.org/">React</a> 
+                  way to learn <a href="https://reactjs.org/">React</a>{' '} 
                   with <a href="https://firebase.google.com">Firebase</a>.
                 </p>
           
-                <a href="/register" className="btn btn-outline-primary mr-2">
+                {user == null && (
+                  <span>
+                  <a href="/register" className="btn btn-outline-primary mr-2">
                   Register
                 </a>
                 <a href="/login" className="btn btn-outline-primary mr-2">
                   Log In
                 </a>
+                  </span>
+                )}
+
+                {user !== null && (
                 <a href="/meetings" className="btn btn-primary">
-                  Meetings
-                </a>
+                Meetings
+              </a>
+                )}
+
               </div>
             </div>
           </div>
