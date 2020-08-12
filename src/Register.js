28b47f4ba73e95b7eb5ab89b2675298bc860f176
Ronen Rokhit <r.rokhit11@gmail.com>
2020-08-11 21:04:25 -0400

    added registration form

--- src/Register.js
+++ src/Register.js
@@ -4,9 +4,77 @@ class Register extends Component {
 
     render() {
         return(
-            <div className="text-center mt-4">
-                <h1 className="text-primary">Register</h1>
+            <form className="mt-3">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-8">
+                  <div className="card bg-light">
+                    <div className="card-body">
+                      <h3 className="font-weight-light mb-3">Register</h3>
+                      <div className="form-row">
+                        <section className="col-sm-12 form-group">
+                          <label
+                            className="form-control-label sr-only"
+                            htmlFor="displayName"
+                          >
+                                Display Name
+                              </label>
+                          <input
+                            className="form-control"
+                            type="text"
+                            id="displayName"
+                            placeholder="Display Name"
+                            name="displayName"
+                            required
+                          />
+                        </section>
+                      </div>
+                      <section className="form-group">
+                        <label
+                          className="form-control-label sr-only"
+                          htmlFor="email"
+                        >
+                          Email
+                        </label>
+                        <input
+                          className="form-control"
+                          type="email"
+                          id="email"
+                          placeholder="Email Address"
+                          required
+                          name="email"
+                        />
+                      </section>
+                      <div className="form-row">
+                        <section className="col-sm-6 form-group">
+                          <input
+                            className="form-control"
+                            type="password"
+                            name="passOne"
+                            placeholder="Password"
+                          />
+                        </section>
+                        <section className="col-sm-6 form-group">
+                          <input
+                            className="form-control"
+                            type="password"
+                            required
+                            name="passTwo"
+                            placeholder="Repeat Password"
+                          />
+                        </section>
+                      </div>
+                      <div className="form-group text-right mb-0">
+                        <button className="btn btn-primary" type="submit">
+                          Register
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </form>
         );
     }
 }
